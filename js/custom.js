23867f9570f5e91670fc9c7c8e2dce0cf1392e04
Mahmoud Elkomy <94573066+namisoftgroup@users.noreply.github.com>
2023-08-31 13:00:13 +0300

    3

--- js/custom.js
+++ js/custom.js
@@ -21,27 +21,27 @@ window.addEventListener("scroll", () => {
     : bar.classList.add("scrolled");
 });
 // swiper-team
-// let aboutSwiper = new Swiper(".teamSwiper", {
-//   spaceBetween: 50,
-//   grabCursor: true,
-//   pagination: true,
-//   loop: true,
-//   pagination: {
-//     el: ".teamSwiperPagination",
-//     clickable: true
-//   },
-//   breakpoints: {
-//     992: {
-//       slidesPerView: 4
-//     },
-//     768: {
-//       slidesPerView: 3
-//     },
-//     350: {
-//       slidesPerView: 1
-//     }
-//   }
-// });
+let aboutSwiper = new Swiper(".teamSwiper", {
+  spaceBetween: 50,
+  grabCursor: true,
+  pagination: true,
+  loop: true,
+  pagination: {
+    el: ".teamSwiperPagination",
+    clickable: true
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 4
+    },
+    768: {
+      slidesPerView: 3
+    },
+    350: {
+      slidesPerView: 1
+    }
+  }
+});
 //contact higlight inputs
 function highlight(el) {
   el.previousElementSibling.classList.add("h");
@@ -118,7 +118,6 @@ var projectsSwiper = new Swiper(".projects", {
   },
 });
 
-
 let toggler = document.querySelector(".toogler");
 let menu = document.querySelector(".nav-links");
 toggler.addEventListener("click", () => {
@@ -168,9 +167,9 @@ $(document).ready(function () {
 });
 $(document).ready(function () {
   //aos Delay
-  $("section").each(function() {
+  $("section").each(function () {
     const sectionDivs = $(this).find("[data-aos]");
-    sectionDivs.each(function(index) {
+    sectionDivs.each(function (index) {
       $(this).attr("data-aos-delay", (index + 1) * 100);
     });
   });
@@ -180,6 +179,38 @@ $(document).ready(function () {
     delay: 50,
     duration: 750,
     // easing: "linear",
-    once: true
+    once: true,
+  });
+  //filter isotop
+  $(document).ready(function () {
+    var $projects = $(".filter-wapper").isotope({
+      itemSelector: ".project-item",
+      layoutMode: "fitRows",
+    });
+    $(".filter-btns").on("click", ".filter-btn", function () {
+      $(".filter-btn").removeClass("active");
+      $(this).addClass("active");
+      var filterValue = $(this).attr("data-filter");
+      $projects.isotope({ filter: filterValue });
+    });
   });
 });
+//contact-map
+if (navigator.geolocation) {
+  var places = [
+    { name: "Toronto, Ontario, Canada", coords: [43.6532, -79.3832] },
+    { name: "Boston, Massachusetts, USA", coords: [42.3601, -71.0589] },
+  ];
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+    const map = L.map("mapLocation").setView([43.0, -75.0], 5);
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+    places.forEach((place) => {
+      L.marker(place.coords).addTo(map).bindPopup(place.name).openPopup();
+    });
+  });
+}
